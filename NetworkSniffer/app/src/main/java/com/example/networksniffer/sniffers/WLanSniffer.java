830fbe6dd842dee596caafd8cac572dbc5d3c13c
CatsArePirates <colin.vanloo@edu.tbz.ch>
2020-12-04 12:56:08 +0100

    Started implementing Pcap

--- NetworkSniffer/app/src/main/java/com/example/networksniffer/sniffers/WLanSniffer.java
+++ NetworkSniffer/app/src/main/java/com/example/networksniffer/sniffers/WLanSniffer.java
@@ -2,6 +2,9 @@ package com.example.networksniffer.sniffers;
 
 import org.jnetpcap.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WLanSniffer implements PacketSniffer{
 
     @Override
@@ -12,5 +15,24 @@ public class WLanSniffer implements PacketSniffer{
     @Override
     public void StopListening() {
 
+    }
+
+    public void Test() {
+        ArrayList<PcapIf> alldevs = new ArrayList();
+        StringBuilder errbuf = new StringBuilder();
+        int r = Pcap.findAllDevs(alldevs, errbuf);
+        System.out.println(r);
+        if (r != Pcap.OK) {
+            System.err.printf("Can't read list of devices, error is %s", errbuf.toString());
+            return;
+        }
+
+        System.out.println("Network devices found:");
+        int i = 0;
+        for (PcapIf device : alldevs) {
+            String description = (device.getDescription() != null) ? device.getDescription()
+                    : "No description aviable";
+            System.out.printf("#%d: %s [%s]\n", i++, device.getName(), description);
+        }
     }
 }
