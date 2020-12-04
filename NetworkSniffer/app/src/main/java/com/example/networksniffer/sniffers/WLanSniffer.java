8efe036170d44edb963225881bbbb32276e5e20a
CatsArePirates <colin.vanloo@edu.tbz.ch>
2020-12-04 09:32:20 +0100

    Implemented jnetpcap

--- NetworkSniffer/app/src/main/java/com/example/networksniffer/sniffers/WLanSniffer.java
+++ NetworkSniffer/app/src/main/java/com/example/networksniffer/sniffers/WLanSniffer.java
@@ -1,10 +1,12 @@
 package com.example.networksniffer.sniffers;
 
+import org.jnetpcap.*;
+
 public class WLanSniffer implements PacketSniffer{
 
     @Override
     public void StartListeningAsync() {
-
+        
     }
 
     @Override
