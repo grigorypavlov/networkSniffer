c32c78842c7542768965887d360fe499bc4e319a
CatsArePirates <colin.vanloo@edu.tbz.ch>
2021-01-08 21:13:08 +0100

    Fix

--- NetworkSniffer/app/src/main/java/com/example/networksniffer/sniffers/BluetoothSniffer.java
+++ NetworkSniffer/app/src/main/java/com/example/networksniffer/sniffers/BluetoothSniffer.java
@@ -3,4 +3,8 @@ package com.example.networksniffer.sniffers;
 
 public class BluetoothSniffer extends PacketSniffer{
 
+    @Override
+    public void Listen() {
+
+    }
 }
--- NetworkSniffer/app/src/main/java/com/example/networksniffer/sniffers/WLanSniffer.java
+++ NetworkSniffer/app/src/main/java/com/example/networksniffer/sniffers/WLanSniffer.java
@@ -3,4 +3,8 @@ package com.example.networksniffer.sniffers;
 
 public class WLanSniffer extends PacketSniffer{
 
+    @Override
+    public void Listen() {
+
+    }
 }
