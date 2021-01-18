5e97728bbc7edb20127b64e5fac96d1cfa54a2be
Colin van Loo <colin.vanloo@edu.tbz.ch>
2021-01-18 10:05:14 +0100

    Added example Unit-test

--- NetworkSniffer/app/src/test/java/com/example/networksniffer/ExampleUnitTest.java
+++ NetworkSniffer/app/src/test/java/com/example/networksniffer/ExampleUnitTest.java
@@ -1,17 +1,21 @@
 package com.example.networksniffer;
 
+import com.example.networksniffer.sniffers.EthernetSniffer;
+import com.example.networksniffer.sniffers.PacketSniffer;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
+ * Unit tests, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void test() {
+        PacketSniffer sniffer = new EthernetSniffer();
+        assertTrue(sniffer.getnInterface() != null);
     }
 }
