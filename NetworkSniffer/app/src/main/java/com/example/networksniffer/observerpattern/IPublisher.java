651cffa05f0a86bc3e63011263a9ec343fd1ca78
Colin van Loo <colin.vanloo@edu.tbz.ch>
2021-01-19 11:21:27 +0100

    Default implemetation for the Observer-Pattern

--- NetworkSniffer/app/src/main/java/com/example/networksniffer/observerpattern/IPublisher.java
+++ NetworkSniffer/app/src/main/java/com/example/networksniffer/observerpattern/IPublisher.java
@@ -1,7 +1,21 @@
 package com.example.networksniffer.observerpattern;
 
+import java.util.ArrayList;
+
 public interface IPublisher {
-    void Subscribe(ISubscriber s);
-    void Unsubscribe(ISubscriber s);
-    void Notify();
+    ArrayList<ISubscriber> subscribers = new ArrayList();
+
+    default void Subscribe(ISubscriber s) {
+        subscribers.add(s);
+    };
+
+    default void Unsubscribe(ISubscriber s) {
+        subscribers.remove(s);
+    };
+
+    default void Notify() {
+        for (ISubscriber s : subscribers) {
+            s.Update(null);
+        }
+    };
 }
