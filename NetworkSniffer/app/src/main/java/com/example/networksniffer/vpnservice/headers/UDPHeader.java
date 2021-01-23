254e5d37bd7f828c08bb2dbce80f80be63158f55
Colin van Loo <colin.vanloo@edu.tbz.ch>
2021-01-23 16:05:45 +0100

    Implemented UDP-Header

---
+++ NetworkSniffer/app/src/main/java/com/example/networksniffer/vpnservice/headers/UDPHeader.java
@@ -0,0 +1,43 @@
+package com.example.networksniffer.vpnservice.headers;
+
+import androidx.annotation.NonNull;
+
+import com.example.networksniffer.vpnservice.BitUtils;
+
+import java.nio.ByteBuffer;
+
+public class UDPHeader {
+    public int sourcePort;
+    public int destinationPort;
+
+    public int length;
+    public int checksum;
+
+    public UDPHeader(ByteBuffer buffer) {
+        this.sourcePort = BitUtils.getUnsignedShort(buffer.getShort());
+        this.destinationPort = BitUtils.getUnsignedShort(buffer.getShort());
+
+        this.length = BitUtils.getUnsignedShort(buffer.getShort());
+        this.checksum = BitUtils.getUnsignedShort(buffer.getShort());
+    }
+
+    public void FillHeader(ByteBuffer buffer) {
+        buffer.putShort((short)this.sourcePort);
+        buffer.putShort((short)this.destinationPort);
+
+        buffer.putShort((short)this.length);
+        buffer.putShort((short)this.checksum);
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("UDPHeader{");
+        sb.append("sourcePort=").append(sourcePort);
+        sb.append(", destinationPort=").append(destinationPort);
+        sb.append(", length=").append(length);
+        sb.append(", checksum=").append(checksum);
+        sb.append('}');
+        return sb.toString();
+    }
+}
