35462489399c73f3400262743dbcc881dc780d4f
Colin van Loo <colin.vanloo@edu.tbz.ch>
2021-01-23 17:11:33 +0100

    Worked on the vpn

--- NetworkSniffer/app/src/main/java/com/example/networksniffer/vpnservice/LocalVPNService.java
+++ NetworkSniffer/app/src/main/java/com/example/networksniffer/vpnservice/LocalVPNService.java
@@ -4,7 +4,11 @@ import android.content.Intent;
 import android.os.ParcelFileDescriptor;
 
 import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 /** Class to connect to a vpn service */
@@ -68,11 +72,83 @@ public class LocalVPNService extends android.net.VpnService {
 
         private FileDescriptor vpnFileDescriptor;
 
-        private ConcurrentLinkedQueue<Packet>
+        private ConcurrentLinkedQueue<Packet> deviceToNetworkUDPQueue;
+        private ConcurrentLinkedQueue<Packet> deviceToNetworkTCPQueue;
+        private ConcurrentLinkedQueue<Packet> networkToDeviceQueue;
+
+        /** Constructor
+         * @param vpnFileDescriptor FileDescriptor
+         * @param deviceToNetworkUDPQueue UDP-Queue
+         * @param deviceToNetworkTCPQueue TCP-Queue
+         * @param networkToDeviceQueue Network-Queue
+         * */
+        public VPNRunnable(FileDescriptor vpnFileDescriptor,
+                           ConcurrentLinkedQueue<Packet> deviceToNetworkUDPQueue,
+                           ConcurrentLinkedQueue<Packet> deviceToNetworkTCPQueue,
+                           ConcurrentLinkedQueue<Packet> networkToDeviceQueue) {
+            this.vpnFileDescriptor = vpnFileDescriptor;
+            this.deviceToNetworkUDPQueue = deviceToNetworkUDPQueue;
+            this.deviceToNetworkTCPQueue = deviceToNetworkTCPQueue;
+            this.networkToDeviceQueue = networkToDeviceQueue;
+        }
 
         /** Is executed when Thread.start() is called */
         public void run() {
-            // TODO: Implement the vpn
+            FileChannel vpnInput = new FileInputStream(vpnFileDescriptor).getChannel();
+            FileChannel vpnOutput = new FileOutputStream(vpnFileDescriptor).getChannel();
+
+            try {
+                ByteBuffer bufferToNetwork = null;
+                boolean dataSent = true;
+                boolean dataReceived;
+
+                while (!Thread.interrupted()) {
+                    if (dataSent)
+                        bufferToNetwork = ByteBufferPool.acquire();
+                    else
+                        bufferToNetwork.clear();
+
+                    int readBytes = vpnInput.read(bufferToNetwork);
+                    if (readBytes > 0) {
+                        dataSent = true;
+                        bufferToNetwork.flip();
+                        Packet packet = new Packet(bufferToNetwork);
+
+                        if (packet.isUDP()) {
+                            deviceToNetworkUDPQueue.offer(packet);
+                        } else if (packet.isTCP()) {
+                            deviceToNetworkTCPQueue.offer(packet);
+                        } else {
+                            // Unknown packet type, discard packet
+                            dataSent = false;
+                        }
+                    } else {
+                        dataSent = false;
+                    }
+
+                    ByteBuffer bufferFromNetwork = networkToDeviceQueue.poll();
+                    if (bufferFromNetwork != null) {
+                        bufferFromNetwork.flip();
+                        while (bufferFromNetwork.hasRemaining()) {
+                            vpnOutput.write(bufferFromNetwork);
+                        }
+                        dataReceived = true;
+                        ByteBufferPool.release(bufferFromNetwork);
+                    } else {
+                        dataReceived = false;
+                    }
+
+                    if (!dataSent && !dataReceived) {
+                        Thread.sleep(10);
+                    }
+                }
+            } catch (InterruptedException iex) {
+                System.out.println("Stopping");
+            } catch (IOException ioex) {
+                System.out.println(ioex.getStackTrace());
+            } finally {
+                CloseResources(vpnInput, vpnOutput);
+            }
         }
     }
 }
